bb36718312189e16cf8873c25b1312fae10775e0
Tutzjwlee <arthurlee@Arthurs-MacBook-Air.local>
2024-09-05 18:33:41 -0300

    trying

---
+++ src/main/java/com/mycompany/armazenvirtual/Armazenvirtual.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.armazenvirtual;
+
+/**
+ *
+ * @author 22.01519-0
+ */
+public class Armazenvirtual {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
